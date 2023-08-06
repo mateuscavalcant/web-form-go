fb43364c1711ef0b2fb5e1a1c7ff7c8da658b4e4
Mateus <mateuscavalcant7@gmail.com>
2023-08-06 14:59:01 -0300

    Add Login Validators

--- src/controller/controller_login.go
+++ src/controller/controller_login.go
@@ -14,35 +14,38 @@ import (
 func UserLogin(c *gin.Context) {
 	var user models.User
 
-	email := strings.TrimSpace(c.PostForm("email"))
+	identifier := strings.TrimSpace(c.PostForm("identifier"))
 	password := strings.TrimSpace(c.PostForm("password"))
 
 	resp := err.ErrorResponse{
 		Error: make(map[string]string),
 	}
 
-	user.Email = email
-	user.Password = password
+	isEmail := strings.Contains(identifier, "@")
+	var queryField string
+	if isEmail {
+		queryField = "email"
+	} else {
+		queryField = "username"
+	}
 
 	db := CON.DB()
 
-	err := db.QueryRow("SELECT id, email, password FROM user1 WHERE email=?", email).Scan(&user.ID, &user.Email, &user.Password)
+	err := db.QueryRow("SELECT id, email, password FROM user1 WHERE "+queryField+"=?", identifier).Scan(&user.ID, &user.Email, &user.Password)
 	if err != nil {
 		log.Println("Error executing SQL statement:", err)
-		resp.Error["email"] = "Invalid credentials"
+		resp.Error["credentials"] = "Invalid credentials"
 	}
 
 	encErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if encErr != nil {
 		resp.Error["password"] = "Invalid password"
-
 	}
+
 	if len(resp.Error) > 0 {
 		c.JSON(400, resp)
 		return
-
 	}
 
-	c.JSON(200, gin.H{"message": "User created successfully"})
-
+	c.JSON(200, gin.H{"message": "User logged in successfully"})
 }
