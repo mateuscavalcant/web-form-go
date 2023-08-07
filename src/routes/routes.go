8e34e1fd80a3c70bd44c74d598dd36875ac957fb
Mateus <mateuscavalcant7@gmail.com>
2023-08-07 13:48:07 -0300

    Add New Route

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -10,4 +10,5 @@ func InitRoutes(r *gin.RouterGroup) {
 	r.POST("/signup", controller.Signup)
 	r.POST("/login", controller.UserLogin)
 	r.POST("/validate-email", controller.ExistEmail)
+	r.POST("/validate-username", controller.ExistUsername)
 }
